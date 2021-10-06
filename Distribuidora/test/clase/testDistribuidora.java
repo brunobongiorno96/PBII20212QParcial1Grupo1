e269ec3dc412abed66b209cbd2737b78d4a5dc3e
valuybruno <valuybruno@192.168.0.163>
2021-10-06 13:07:23 -0300

    commit de creacion

---
+++ Distribuidora/test/clase/testDistribuidora.java
@@ -0,0 +1,14 @@
+package clase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testDistribuidora {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
